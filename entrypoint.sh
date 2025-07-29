dac4031eb34e040d83061e845aaa720e681236c3
asdf57 <53836417+asdf57@users.noreply.github.com>
2025-07-28 22:14:44 -0400

    Fix entrypoint so that it works in docker-compose

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,11 @@ eval "$(ssh-agent -s)" >/dev/null 2>&1
 
 mkdir -p /root/.ssh
 
+if [[ -f /run/secrets/git_provisioning_key ]]; then
+    echo "Detected that we are running in docker-compose!"
+    SSH_KEY=$(cat /run/secrets/git_provisioning_key)
+fi
+
 echo "$SSH_KEY" > /root/.ssh/git_provisioning_key
 chmod 600 /root/.ssh/git_provisioning_key
 ssh-add /root/.ssh/git_provisioning_key
